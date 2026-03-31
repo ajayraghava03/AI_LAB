67e30dcc69ea600083739f0785207fd0f3f8d58e
Ajay raghava Kodem <154720431+ajayraghava03@users.noreply.github.com>
2026-03-31 09:16:36 +0530

    Refactor ALL.py to improve structure and readability

--- ALL.py
+++ ALL.py
@@ -96,94 +96,98 @@ if not winner:
 # -------------------------------
 # 4. 8 Puzzle
 # -------------------------------
-def eight_puzzle():
-    goal = "123456780"
-    start = input("Enter initial state (use 0 for blank): ")
 
-    def get_neighbors(state):
-        moves = []
-        i = state.index('0')
-        swaps = {
-            0:[1,3],1:[0,2,4],2:[1,5],
-            3:[0,4,6],4:[1,3,5,7],5:[2,4,8],
-            6:[3,7],7:[4,6,8],8:[5,7]
-        }
-        for j in swaps[i]:
-            lst = list(state)
-            lst[i], lst[j] = lst[j], lst[i]
-            moves.append("".join(lst))
-        return moves
+def print_board(board):
+    for i in range(0, 9, 3):
+        print(board[i], board[i+1], board[i+2])
+    print()
 
-    queue = deque([(start, [])])
-    visited = set()
+board = [1,2,3,4,0,6,7,5,8]
 
-    while queue:
-        state, path = queue.popleft()
-        if state == goal:
-            print("Solved Path:", path)
-            return
-        visited.add(state)
-        for n in get_neighbors(state):
-            if n not in visited:
-                queue.append((n, path+[n]))
+while True:
+    print_board(board)
 
+    if board == [1,2,3,4,5,6,7,8,0]:
+        print("Puzzle Solved!")
+        break
+
+    move = input("Move (w=up, s=down, a=left, d=right): ")
+
+    pos = board.index(0)
+
+    if move == "w" and pos not in [0,1,2]:
+        board[pos], board[pos-3] = board[pos-3], board[pos]
+
+    elif move == "s" and pos not in [6,7,8]:
+        board[pos], board[pos+3] = board[pos+3], board[pos]
+
+    elif move == "a" and pos not in [0,3,6]:
+        board[pos], board[pos-1] = board[pos-1], board[pos]
+
+    elif move == "d" and pos not in [2,5,8]:
+        board[pos], board[pos+1] = board[pos+1], board[pos]
+
+    else:
+        print("Invalid move!")
+
+   
 
 # -------------------------------
 # 5. Water Jug
 # -------------------------------
-def water_jug():
-    x = int(input("Enter capacity of jug1: "))
-    y = int(input("Enter capacity of jug2: "))
-    target = int(input("Enter target: "))
 
-    visited = set()
-    stack = [(0,0)]
+x = int(input("Enter capacity of jug1: "))
+y = int(input("Enter capacity of jug2: "))
+target = int(input("Enter target: "))
 
-    print("Steps:")
-    while stack:
-        a, b = stack.pop()
-        if (a,b) in visited:
-            continue
-        visited.add((a,b))
-        print(a, b)
+visited = set()
+stack = [(0,0)]
 
-        if a == target or b == target:
-            print("Target reached")
-            return
+print("Steps:")
+while stack:
+    a, b = stack.pop()
+    if (a,b) in visited:
+        continue
+    visited.add((a,b))
+    print(a, b)
 
-        stack.extend([
-            (x,b),(a,y),(0,b),(a,0),
-            (max(0,a-(y-b)), min(y,b+a)),
-            (min(x,a+b), max(0,b-(x-a)))
-        ])
+    if a == target or b == target:
+        print("Target reached")
+        return
+
+    stack.extend([
+        (x,b),(a,y),(0,b),(a,0),
+        (max(0,a-(y-b)), min(y,b+a)),
+        (min(x,a+b), max(0,b-(x-a)))
+    ])
 
 
 # -------------------------------
 # 6. TSP
 # -------------------------------
-def tsp():
-    n = int(input("Enter number of cities: "))
-    graph = []
 
-    print("Enter cost matrix:")
-    for _ in range(n):
-        row = list(map(int, input().split()))
-        graph.append(row)
+n = int(input("Enter number of cities: "))
+graph = []
 
-    cities = list(range(n))
-    min_cost = float('inf')
-    bp=None
-    for perm in permutations(cities):
-        cost = 0
-        path=(0,)+perm(0, )
-        for i in range(len(path)-1):
-            cost+=graph[path[i]][path[i+1]]
-        if cost<min_cost:
-            min_cost=cost
-            bp=path
+print("Enter cost matrix:")
+for _ in range(n):
+    row = list(map(int, input().split()))
+    graph.append(row)
 
-    print("Minimum Cost:", min_cost)
-    print("Best Path:", bp)
+cities = list(range(n))
+min_cost = float('inf')
+bp=None
+for perm in permutations(cities):
+    cost = 0
+    path=(0,)+perm(0, )
+    for i in range(len(path)-1):
+        cost+=graph[path[i]][path[i+1]]
+    if cost<min_cost:
+        min_cost=cost
+        bp=path
+
+print("Minimum Cost:", min_cost)
+print("Best Path:", bp)
 
 
 # -------------------------------
@@ -197,6 +201,5 @@ def hanoi(n, source, target, auxiliary):
     print(f"Move disk {n} from {source} to {target}")
     hanoi(n-1, auxiliary, target, source)
 
-def run_hanoi():
-    n = int(input("Enter number of disks: "))
-    hanoi(n, 'A', 'C', 'B')
+n = int(input("Enter number of disks: "))
+hanoi(n, 'A', 'C', 'B')
