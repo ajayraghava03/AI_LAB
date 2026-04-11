1e41ed66a849d78df7ffebf0e7ad5c62730ee389
Ajay raghava Kodem <154720431+ajayraghava03@users.noreply.github.com>
2026-04-11 16:42:45 +0530

    Create BFS.py

---
+++ BFS.py
@@ -0,0 +1,27 @@
+# 1. BFS (User Input Graph)
+# -------------------------------
+from collections import deque
+
+graph = {}
+n = int(input("Enter number of nodes: "))
+
+for _ in range(n):
+    node = input("Enter node: ")
+    neighbors = input(f"Enter neighbors of {node} (space separated): ").split()
+    graph[node] = neighbors
+
+start = input("Enter start node: ")
+
+visited = set()
+queue = deque([start])
+
+print("BFS Traversal:")
+while queue:
+    node = queue.popleft()
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbor in graph.get(node,[]):
+            if neighbor not in visited:
+                queue.append(neighbor)
+print()
